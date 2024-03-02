73432ec5cdd6040fd5f268d48007b291d539d205
tamtaSaurabh <154445139+tamtaSaurabh@users.noreply.github.com>
2024-03-02 17:12:27 +0530

    Add files via upload

---
+++ typeconversion.js
@@ -0,0 +1,23 @@
+//"33" => 33
+//"33abc"=> Nan
+//true=>1 false=>0
+let score =true
+console.log(typeof score)
+
+let valueNUmber = Number(score)
+console.log(typeof valueNUmber);
+console.log(valueNUmber);
+
+
+let num = "sauraabh"
+let booleannum = Boolean(num);
+console.log(typeof booleannum);
+console.log(booleannum);
+// 1 => true; 0=>false
+//""=>false
+//"saurabh"=>true
+
+let sumnuber = 33
+let strngnum = String(sumnuber);
+console.log(typeof strngnum);
+console.log(strngnum);
